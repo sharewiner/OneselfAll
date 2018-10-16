c324ad494a0750b1c4b309cbd6b19085c102256a
sharewiner <603032132@qq.com>
2018-10-16 10:50:49 +0800

    201810161050

---
+++ app/src/main/java/com/example/shaohui/oneselfall/common/clock/CountShowedClockView.java
@@ -0,0 +1,168 @@
+package com.example.shaohui.oneselfall.common.clock;
+
+import android.annotation.SuppressLint;
+import android.content.Context;
+import android.content.res.TypedArray;
+import android.graphics.drawable.Drawable;
+import android.util.AttributeSet;
+import android.view.View;
+import android.widget.HorizontalScrollView;
+import android.widget.LinearLayout;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
+
+import com.example.shaohui.oneselfall.R;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class CountShowedClockView extends HorizontalScrollView {
+    private List<FlipClockView> flipClockViewList = new ArrayList<>();
+
+    String[] startNumber;
+    String[] changeNumber;
+    private String startNumberValue = "";
+    private String changeNumberValue = "";
+    private float textSize;
+    private float viewWidth;
+    private float viewHeight;
+    LinearLayout linearLayout;
+    private LayoutParams mLayoutParams;
+
+    private int clockCount = 1;
+
+    private int space;
+    private float countSpace;
+
+    public CountShowedClockView(Context context) {
+        this(context, null);
+    }
+
+    public CountShowedClockView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+        initView(context, attrs);
+    }
+
+    public CountShowedClockView(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+        initView(context, attrs);
+    }
+
+    @SuppressLint("NewApi")
+    public void initView(Context context, AttributeSet attrs) {
+        TypedArray tArray = context.obtainStyledAttributes(attrs,
+                R.styleable.NewMyClock);
+        textSize = tArray.getDimension(R.styleable.NewMyClock_countTextSize, 26f);
+        int textColor = tArray.getColor(R.styleable.NewMyClock_countTextColor, 0xffffff);
+        Drawable textBg = tArray.getDrawable(R.styleable.NewMyClock_countTextBackground);
+        clockCount = tArray.getInteger(R.styleable.NewMyClock_countShowNumber, 1);
+
+        countSpace = tArray.getDimension(R.styleable.NewMyClock_countSpace, 70);
+        viewWidth = tArray.getDimension(R.styleable.NewMyClock_countWidth, 100);
+        viewHeight = tArray.getDimension(R.styleable.NewMyClock_countHeight, 80);
+        space = (int) countSpace;
+
+        tArray.recycle();
+
+        startNumber = new String[clockCount];
+        changeNumber = new String[clockCount];
+
+        linearLayout = new LinearLayout(context);
+        mLayoutParams = new LayoutParams(LayoutParams.MATCH_PARENT, LayoutParams.WRAP_CONTENT);
+        linearLayout.setOrientation(LinearLayout.HORIZONTAL);
+        linearLayout.setLayoutParams(mLayoutParams);
+        addView(linearLayout);
+
+        flipClockViewList.clear();
+        if (clockCount > 0) {
+            for (int i = 0; i < clockCount; i++) {
+
+                startNumber[i] = "0";
+                changeNumber[i] = "0";
+
+                FlipClockView flipClockView = new FlipClockView(context);
+                flipClockView.setId(View.generateViewId());
+//                flipClockView.setClockBackground(textBg);
+                flipClockView.setClockBackground(context.getResources().getDrawable(R.drawable.ic_clock_bg));
+                flipClockView.setClockTextSize(textSize);
+                flipClockView.setClockTextColor(textColor);
+                flipClockView.setClockTime("0");
+                LinearLayout.LayoutParams lp = new LinearLayout.LayoutParams((int) viewWidth, (int) viewHeight);
+                flipClockView.setLayoutParams(lp);
+
+                if (i == 1 || i == 4 || i == 7) {
+                    lp.setMargins(space, 0, space, space * (7/4));
+                } else {
+                    lp.setMargins(0, 0, 0, space * (7/4));
+                }
+
+                linearLayout.addView(flipClockView);
+                flipClockViewList.add(flipClockView);
+
+                if (i == 2 || i == 5) {
+                    RelativeLayout relativeLayout = new RelativeLayout(context);
+                    LinearLayout.LayoutParams lprl = new LinearLayout.LayoutParams(LayoutParams.WRAP_CONTENT, LayoutParams.MATCH_PARENT);
+                    relativeLayout.setLayoutParams(lprl);
+
+                    lprl.setMargins((int) (space * 2.5), 0, 0, 0);
+                    TextView textView = new TextView(context);
+                    textView.setText("，");
+                    textView.setTextSize(textSize);
+                    textView.setTextColor(getResources().getColor(R.color.gay));
+
+                    RelativeLayout.LayoutParams lptv = new RelativeLayout.LayoutParams(LayoutParams.WRAP_CONTENT, LayoutParams.WRAP_CONTENT);
+                    lptv.addRule(RelativeLayout.ALIGN_PARENT_BOTTOM);
+                    textView.setLayoutParams(lptv);
+                    relativeLayout.addView(textView);
+
+                    linearLayout.addView(relativeLayout);
+                }
+            }
+        }
+    }
+
+    /**
+     * 获取布局宽度
+     */
+    public int getScreenWidth(Context mContext) {
+        return mContext.getResources().getDisplayMetrics().widthPixels;
+    }
+
+    /**
+     * 传入修改数据
+     */
+    public void setChangeNumber(String number) {
+        changeNumberValue = number;
+        if (!"".equals(changeNumberValue)) {
+            int count = changeNumberValue.length();
+            for (int i = 0; i < count; i++) {
+                int a = clockCount - i - 1;
+                changeNumber[a] = changeNumberValue.substring(count - i - 1, count - i);
+            }
+        }
+
+        setRefreshNumber();
+    }
+
+    /**
+     * 修改滑动
+     */
+    public void setRefreshNumber() {
+        for (int i = 0; i < clockCount; i++) {
+            int a = clockCount - i - 1;
+            if (!changeNumber[a].equals(startNumber[a])) {
+                startNumber[a] = changeNumber[a];
+
+                flipClockViewList.get(a).setClockTime(changeNumber[a]);
+                flipClockViewList.get(a).smoothFlip();
+
+            }
+        }
+
+        startNumberValue = changeNumberValue;
+        changeNumberValue = "";
+    }
+
+
+}
+
---
+++ app/src/main/java/com/example/shaohui/oneselfall/common/clock/MyTextView.java
@@ -0,0 +1,70 @@
+package com.example.shaohui.oneselfall.common.clock;
+
+import android.annotation.SuppressLint;
+import android.content.Context;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.graphics.RectF;
+import android.util.AttributeSet;
+import android.widget.TextView;
+
+import com.example.shaohui.oneselfall.R;
+
+@SuppressLint("AppCompatCustomView")
+public class MyTextView extends TextView {
+    Context context;
+    Paint mPaint;
+    int color;
+
+    public MyTextView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+        this.context = context;
+        initPaint();
+    }
+
+    public MyTextView(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+        initPaint();
+    }
+
+    public MyTextView(Context context, int colorPrimaryDark) {
+        this(context, null);
+        color = colorPrimaryDark;
+        initPaint();
+    }
+
+    private void initPaint() {
+        mPaint = new Paint(Paint.ANTI_ALIAS_FLAG);
+        mPaint.setStyle(Paint.Style.FILL);
+        mPaint.setARGB(255, 0, 0, 0);//设置Paint对象颜色，参数一为alpha透明通道
+        mPaint.setAlpha(255);
+        mPaint.setColor(color);
+    }
+
+    @Override
+    protected void onDraw(Canvas canvas) {
+        int radius = getMeasuredWidth() / 5;
+        int radiusCut = radius - 3;
+        int radiusAdd = radius + 3;
+        int space = radius / 6;
+
+//        canvas.drawRect(0, getMeasuredHeight() / 2 - space, getMeasuredWidth(), getMeasuredHeight() / 2 + space, mPaint);
+////        // 设置个新的长方形，60为左上点的x坐标，100为左上点的y坐标；200为右下点的 x坐标，240为右下点的y坐标。
+////        RectF oval1 = new RectF(-radiusCut
+////                , getMeasuredHeight() / 2 - radiusAdd
+////                , radiusCut
+////                , getMeasuredHeight() / 2 + radiusAdd);
+////
+////        RectF oval2 = new RectF(getMeasuredWidth() - radiusCut
+////                , getMeasuredHeight() / 2 - radiusAdd,
+////                getMeasuredWidth() + radiusCut
+////                , getMeasuredHeight() / 2 + radiusAdd);
+////        // 画弧，第一个参数是RectF：该类是第二个参数是角度的开始，第三个参数是多少度，第四个参数是真的时候画扇形，是假的时候画弧     线
+////        canvas.drawArc(oval1, 270, 180, true, mPaint);
+////        canvas.drawArc(oval2, 90, 180, true, mPaint);
+        super.onDraw(canvas);
+    }
+
+}
+
